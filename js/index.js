b5e02791b78986a0ba781c81add311777e2e0d9c
Lesfer Ayoub <lesferayoub@gmail.com>
2017-09-01 08:23:06 +0200

    Integrated Twitter button

--- js/index.js
+++ js/index.js
@@ -2,24 +2,31 @@ $(document).ready(function(){
   quoteFade();
 
   $("#newquote-btn").on("click", quoteFade);
+  $(".mycontainer").hover(quoteTweet, function(){
+    $(".quote-tweet").css("visibility","hidden");
+  });
 
-  timer = 0;
   function quoteFade(){
+    timer = 0;
     $.getJSON("js/quotes.json", function(json){
       clearTimeout(timer);
       var html = "";
       var totalQuoteNumber = json.quotes.length;
-      console.log(totalQuoteNumber);
       var randomId = Math.floor(Math.random() * totalQuoteNumber);
 
       html += "<div class='quote'>";
-      html += "<p class='quote-body'>" + json.quotes[randomId].body + "<br>";
-      html += "<span class='author'>" + json.quotes[randomId].author + "</span></p>";
+      html += "<p class='quote-body'><span class='quote-text'>" + json.quotes[randomId].body + "</span><br>";
+      html += "<span class='quote-author'>" + json.quotes[randomId].author + "</span></p>";
       html += "</div>";
 
       $("#quote-container").html(html);
       $(".quote").hide().fadeIn(2000);
       timer = setTimeout(quoteFade, 10000);
     });
+  }
+
+  function quoteTweet(){
+    $(".twitter-share-button").attr("href", "https://twitter.com/share?text=" + encodeURIComponent("\"" + $(".quote-text").text() + "\" - " + $(".quote-author").text()));
+    $(".quote-tweet").css("visibility","visible");
   }
 });
